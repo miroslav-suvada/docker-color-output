c7e1d85d1cdfd4c589cb4bf70e6e3385bc43d8b0
Miroslav Šuvada <miroslav.suvada@gmail.com>
2023-07-04 07:17:59 +0200

    Override colors with variables

--- pkg/color/color.go
+++ pkg/color/color.go
@@ -2,22 +2,22 @@ package color
 
 const (
 	reset       = "\033[0m"
-	black       = "\033[0;30m"
-	darkGray    = "\033[1;30m"
-	red         = "\033[0;31m"
-	lightRed    = "\033[1;31m"
-	green       = "\033[0;32m"
-	lightGreen  = "\033[1;32m"
-	brown       = "\033[0;33m"
-	yellow      = "\033[1;33m"
-	blue        = "\033[0;34m"
-	lightBlue   = "\033[1;34m"
-	purple      = "\033[0;35m"
-	lightPurple = "\033[1;35m"
-	cyan        = "\033[0;36m"
-	lightCyan   = "\033[1;36m"
-	lightGray   = "\033[0;37m"
-	white       = "\033[1;37m"
+	black       = "${DOCKER_COLOR_BLACK:-\033[0;30m"}
+	darkGray    = "${DOCKER_COLOR_DARKGRAY:-\033[1;30m}"
+	red         = "${DOCKER_COLOR_RED:-\033[0;31m}"
+	lightRed    = "${DOCKER_COLOR_LIGHTRED:-\033[1;31m}"
+	green       = "${DOCKER_COLOR_GREEN:-\033[0;32m}"
+	lightGreen  = "${DOCKER_COLOR_LIGHTGREEN:-\033[1;32m}"
+	brown       = "${DOCKER_COLOR_BROWN:-\033[0;33m}"
+	yellow      = "${DOCKER_COLOR_YELLOW:-\033[1;33m}"
+	blue        = "${DOCKER_COLOR_BLUE:-\033[0;34m}"
+	lightBlue   = "${DOCKER_COLOR_LIGHTBLUE:-\033[1;34m}"
+	purple      = "${DOCKER_COLOR_PURPLE:-\033[0;35m}"
+	lightPurple = "${DOCKER_COLOR_LIGHTPURPLE:-\033[1;35m}"
+	cyan        = "${DOCKER_COLOR_CYAN:-\033[0;36m}"
+	lightCyan   = "${DOCKER_COLOR_LIGHTCYAN:-\033[1;36m}"
+	lightGray   = "${DOCKER_COLOR_LIGHTGRAY:-\033[0;37m}"
+	white       = "${DOCKER_COLOR_WHITE:-\033[1;37m}"
 )
 
 func Black(value string) string {
